55240b2a73614193f9e910fcd00c2bfe8f9004a7
ElizabethR20 <eroch2093@gmail.com>
2016-12-11 17:18:58 -0600

    Update Programa_Raspberry.py

--- Programa_Raspberry.py
+++ Programa_Raspberry.py
@@ -14,32 +14,44 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 b = 0
 i = 0
 f = 0
+v = 0
 while True:
 	arduino = int(ser.readline())
 	if arduino == 0:
 		i = 0
 		f = 0
+		v = 0
 		if b == 0: 
 			b = 1
 			print("Todo Bien")
 	elif arduino == 1:
 		b = 0
 		f = 0
+		v = 0
 		if i == 0:
 			i = 1
 			print("Hay un Intruso")
-			cap = capturando.get_thread()
-			if cap:
-				cap.start()
-			else:
-				print("no obtuvo thread")
 			alertas.intr_alert(alertas.eliaddr)
 			alertas.intr_alert(alertas.uriaddr)
 	elif arduino == 2:
 		b = 0
 		i = 0
+		v = 0
 		if f == 0:
 			f = 1
 			print ("Hay un Incendio")
 			alertas.fire_alert(alertas.eliaddr)
 			alertas.fire_alert(alertas.uriaddr)
+	elif arduino == 3 or arduino == 4:
+		b = 0
+		i = 0
+		f = 0
+		if v == 0:
+			v = 1
+			cap = capturando.get_thread()
+                	if cap:
+                		cap.start()
+                	else:
+				print("no obtuvo thread")
+
+
